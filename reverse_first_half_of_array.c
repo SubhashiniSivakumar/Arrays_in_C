8530aebe81afb80b1e1377c384db7ce75f875007
Subhashini S <subhashinisiva16@gmail.com>
2025-09-29 20:10:44 +0530

    Create reverse_first_half_of_array.c
    Problem Statement:
    Complete the code snippet below to reverse the first half of the array (in place replacement).
    
    Input:
    Integer Integers
    
    Output:
    Integers
    
    Sample Input 1
    12 1 2 3 4 5 6 7 8 9 10 11 12
    
    Sample Output 1
    6 5 4 3 2 1 7 8 9 10 11 12
    
    Sample Input 2
    12 10 20 30 40 50 60 70 80 90 100 110 120
    
    Sample Output 2
    60 50 40 30 20 10 70 80 90 100 110 120

---
+++ reverse_first_half_of_array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int N, start, end, index, temp;
+    scanf("%d",&N);
+    int arr[N];
+    for(index=0;index<N;index++)
+    {
+        scanf("%d",&arr[index]);
+    }
+    //Write your code here
+    start=0;
+    end=(N-1)/2;
+    while(start<end){
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+    
+    start++;
+    end--;
+    }
+    
+   //Your code ends here
+   for(index=0;index<N;index++)
+        printf("%d ",arr[index]);
+    return 0;
+}
